52867f2c4b87059be7fbb3857c7224c541d26ab1
Hermes <hermessantos995@gmail.com>
2023-09-04 18:27:01 -0300

    chamging the theme

--- init.lua
+++ init.lua
@@ -128,15 +128,40 @@ require('lazy').setup({
       end,
     },
   },
-
-  {
-    'rose-pine/neovim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'rose-pine'
-    end,
-  },
-
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		opts = {
+			term_colors = true,
+			transparent_background = false,
+			styles = {
+				comments = {},
+				conditionals = {},
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+			},
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
+				},
+			},
+		},
+	},
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
